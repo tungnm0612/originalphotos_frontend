46809ea9282192a59e8f7018359520963733a35d
TungNguyen <tungnm52@wru.vn>
2019-12-30 00:38:46 +0700

    update 30/12/2019 part 2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   rootPath:
-    // process.env.REACT_APP_STATE === "localhost" ? "http://localhost:8000" : "http://ec2-52-22-56-26.compute-1.amazonaws.com:8000"
-    process.env.REACT_APP_STATE === "localhost" ? "http://localhost:8000" : "http://localhost:8000"
+    process.env.REACT_APP_STATE === "localhost" ? "http://localhost:8000" : "http://ec2-52-22-56-26.compute-1.amazonaws.com:8000"
+    // process.env.REACT_APP_STATE === "localhost" ? "http://localhost:8000" : "http://localhost:8000"
 };
