ffc0d30ce25b8c9a64a57a17dda4e1fc380e662e
TungNguyen <tungnm52@wru.vn>
2020-01-06 17:36:06 +0700

    update 6/1/2019

--- src/containers/AddUserScreen.js
+++ src/containers/AddUserScreen.js
@@ -3,17 +3,17 @@ import axios from 'axios';
 import config from '../config';
 
 export default class AddUserScreen extends Component {
-    onSubmit = (event) =>{
+    onSubmitAddUser = (event) =>{
         event.preventDefault();
-        const password = document.getElementById("inputPassword").value;
-        const retypepassword = document.getElementById("retypePassword").value
+        const password = document.getElementById("inputPasswordAdd").value;
+        const retypepassword = document.getElementById("retypePasswordAdd").value
         if (password === retypepassword) {
             axios.post(config.rootPath + "/api/users/adduser", {
-                fullname: document.getElementById("inputFullname").value,
-                email: document.getElementById("inputEmail").value,
-                username: document.getElementById("inputUsername").value,
-                password: document.getElementById("inputPassword").value,
-                phone: document.getElementById('inputPhone').value
+                fullname: document.getElementById("inputFullnameAdd").value,
+                email: document.getElementById("inputEmailAdd").value,
+                username: document.getElementById("inputUsernameAdd").value,
+                password: document.getElementById("inputPasswordAdd").value,
+                phone: document.getElementById('inputPhoneAdd').value
             }).then(res =>{
                 alert(res.data.message);
                 if (res.data.message === "Tạo tài khoản thành công!") {
@@ -43,30 +43,30 @@ export default class AddUserScreen extends Component {
                             </button>
                         </div>
                         <div className="modal-body">
-                        <form onSubmit = {this.onSubmit} className="form" >
+                        <form onSubmit = {this.onSubmitAddUser} className="form" >
                             <div className="form-group">
-                                <label htmlFor="inputFullname" className="col-form-label">Họ và Tên</label>
-                                <input type="text" className="form-control" id="inputFullname" required/>
+                                <label htmlFor="inputFullnameAdd" className="col-form-label">Họ và Tên</label>
+                                <input type="text" className="form-control" id="inputFullnameAdd" required/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="inputEmail" className="col-form-label">Email</label>
-                                <input type="email" className="form-control" id="inputEmail" required/>
+                                <label htmlFor="inputEmailAdd" className="col-form-label">Email</label>
+                                <input type="email" className="form-control" id="inputEmailAdd" required/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="inputPhone" className="col-form-label">Số điện thoại</label>
-                                <input type="tel" pattern="[0-9]{10}" className="form-control" id="inputPhone" required/>
+                                <label htmlFor="inputPhoneAdd" className="col-form-label">Số điện thoại</label>
+                                <input type="tel" pattern="[0-9]{10}" className="form-control" id="inputPhoneAdd" required/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="inputUsername" className="col-form-label">Tài khoản</label>
-                                <input type="text" className="form-control" id="inputUsername" required/>
+                                <label htmlFor="inputUsernameAdd" className="col-form-label">Tài khoản</label>
+                                <input type="text" className="form-control" id="inputUsernameAdd" required/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="inputPassword" className="col-form-label">Mật khẩu</label>
-                                <input type="password" className="form-control" id="inputPassword" required/>
+                                <label htmlFor="inputPasswordAdd" className="col-form-label">Mật khẩu</label>
+                                <input type="password" className="form-control" id="inputPasswordAdd" required/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="retypePassword" className="col-form-label">Nhập lại mật khẩu</label>
-                                <input type="password" className="form-control" id="retypePassword" required/>
+                                <label htmlFor="retypePasswordAdd" className="col-form-label">Nhập lại mật khẩu</label>
+                                <input type="password" className="form-control" id="retypePasswordAdd" required/>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-dismiss="modal">Hủy</button>
